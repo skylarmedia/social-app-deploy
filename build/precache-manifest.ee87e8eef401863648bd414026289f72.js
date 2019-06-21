dc1c7d759d1f8f8870b1b955b9fe23f52b47161b
Edward Lanto <edwardlanto@EdwardMac-Pro.local>
2019-06-21 15:58:20 -0400

    commit build

---
+++ build/precache-manifest.ee87e8eef401863648bd414026289f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c031ef82bb70b3ae9be059f9e9e4be0",
+    "url": "/social-app-deploy/index.html"
+  },
+  {
+    "revision": "29ac51133015ad02406f",
+    "url": "/social-app-deploy/static/css/2.49d8c5c4.chunk.css"
+  },
+  {
+    "revision": "a36089fc616c5810786c",
+    "url": "/social-app-deploy/static/css/main.444c250e.chunk.css"
+  },
+  {
+    "revision": "29ac51133015ad02406f",
+    "url": "/social-app-deploy/static/js/2.4297df1c.chunk.js"
+  },
+  {
+    "revision": "a36089fc616c5810786c",
+    "url": "/social-app-deploy/static/js/main.211b9e4d.chunk.js"
+  },
+  {
+    "revision": "4b23a16d05c1a7d0d574",
+    "url": "/social-app-deploy/static/js/runtime~main.7a0b50cb.js"
+  }
+]);
