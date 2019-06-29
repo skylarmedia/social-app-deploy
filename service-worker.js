3b847e28bb314ffbda9e9f8d4b2f8f6c2bc93541
Edward Lanto <edwardlanto@EdwardMac-Pro.local>
2019-06-29 12:43:45 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-app-deploy/precache-manifest.ae3a7307df74004a0d00e853ca9c0fca.js"
+  "/social-app-deploy/precache-manifest.eb64d66ac52f34112ffc3ac03335b1b7.js"
 );
 
 self.addEventListener('message', (event) => {
